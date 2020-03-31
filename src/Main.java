c6c14df194c51374f217f08ba1aff9ce0f02eb54
Luis Carlos Parra Mendoza <47015841+LuisCaParra@users.noreply.github.com>
2020-03-30 21:31:09 -0700

    init

---
+++ src/Main.java
@@ -0,0 +1,133 @@
+import java.util.Scanner;
+
+public class Main {
+    static int posicion=0;
+    static String[] cadena;
+    static int current = 0;
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Introduce tu cadena ");
+        String input = in.nextLine();
+        cadena = input.split("");
+       int r = EXP();
+        System.out.println("Tu cadena es: ");
+        checkString (r,posicion);
+    }
+
+   static void checkString ( int r,int pos){
+        if(r==1 && pos==1){
+            System.out.println("correct");
+        }else{
+            System.out.println("incorrect");
+        }
+    }
+
+    static int EXP(){
+        int p=posicion;
+
+        String c;
+
+        if(TERM()==1){
+            if(cadena.length <=posicion){
+                posicion=1;
+                return 1;
+            };
+            c=cadena[posicion++];
+            if(c.equals("+") ){
+                if(EXP()==1){
+                    return 1;
+                }
+            }
+        }
+        posicion = p;
+        if(TERM()==1){
+            c=cadena[posicion++];
+            if(c.equals("-") ){
+                if(EXP()==1){
+                    return 1;
+                }
+            }
+        }
+        posicion = p;
+        if(TERM()==1){
+            return 1;
+        }
+        posicion = p;
+        return 0;
+    }
+
+
+    static int TERM(){
+        int p=posicion;
+        String c;
+        if(FACT()==1){
+            return 1;
+        }
+
+        posicion = p;
+        if(FACT()==1){
+
+            c=cadena[posicion++];
+            if(c.equals("*")){
+                if(TERM()==1){
+                    return 1;
+                }
+            }
+        }
+        posicion = p;
+        if(FACT()==1){
+            c=cadena[posicion++];
+            if(c.equals("/")){
+                if(TERM()==1){
+                    return 1;
+                }
+            }
+        }
+
+        posicion = p;
+        return 0;
+    }
+
+    static int FACT(){
+        int p=posicion;
+        String c;
+        if(NUM()==1){
+            return 1;
+        }
+        posicion = p;
+        c=cadena[posicion++];
+        if(c.equals("(")){
+            if(EXP()==1){
+                c=cadena[posicion++];
+                if(c.equals(")")){
+                    return 1;
+                }
+            }
+        }
+        posicion = p;
+        c=cadena[posicion++];
+        if(c.equals("-")){
+            if(FACT()==1){
+                return 1;
+            }
+        }
+        posicion = p;
+        return 0;
+
+    }
+
+    static int NUM(){
+        int p=posicion;
+
+        String c;
+        c=cadena[posicion++];
+        if(c.equals("0") ||c.equals("1") ||c.equals("2") ||c.equals("3") ||c.equals("4") ||c.equals("5")||c.equals("6") ||c.equals("7") ||c.equals("8") ||c.equals("9")){
+            return 1;
+        }
+        posicion = p;
+        return 0;
+
+    }
+
+}
